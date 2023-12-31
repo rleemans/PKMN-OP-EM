8b45db318f7f70ec9ec8e797307bfa78342121e4
Rob Leemans <93993725+rleemans@users.noreply.github.com>
2023-12-31 15:27:57 +0100

    mud

--- src/data/pokemon/species_info/gen_3.h
+++ src/data/pokemon/species_info/gen_3.h
@@ -472,7 +472,7 @@ const struct SpeciesInfo gSpeciesInfoGen3[] =
         ICON(Marshtomp, 0),
         .footprint = gMonFootprint_Marshtomp,
         LEARNSETS(Marshtomp),
-        .evolutions = EVOLUTION({EVO_ITEM, ITEM_POTION, SPECIES_SWAMPERT}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 8, SPECIES_SWAMPERT}),
     },
 
 #define SWAMPERT_MISC_INFO                                      \
